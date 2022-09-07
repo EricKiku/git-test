3489917c07c2567398dbcc3f35b2905e41d48815
EricKiku <93976625+EricKiku@users.noreply.github.com>
2022-09-07 20:31:47 +0800

    Update GitTest.java

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("我又回来了2");
         System.out.println("我又回来了-hot-fix");
         System.out.println("我要推送代码了");
+        System.out.println("我是要拉取的代码~");
     }
 }
